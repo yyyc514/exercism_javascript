c3a993259e9f27b623e3cba09e4c6abd021eb333
Josh Goebel <me@joshgoebel.com>
2019-08-27 17:27:37 -0400

    more object oriented

--- queen-attack/queen-attack.js
+++ queen-attack/queen-attack.js
@@ -1,52 +1,75 @@
-const sameSpace = (piece, other) => piece[0] == other[0] && piece[1] == other[1]
 const BOARD_EDGE = 8
+
+class Square {
+    constructor([rank,file]) {
+        this.rank = rank
+        this.file = file
+    }
+
+    sameSquare(other) {
+        return this.sameFile(other) && this.sameRank(other)
+    }
+
+    sameDiagonal(other) {
+        return Math.abs(this.file-other.file) ==
+            Math.abs(this.rank-other.rank)
+    }
+
+    sameRank(other) {
+        return this.rank == other.rank
+    }
+
+    sameFile(other) {
+        return this.file == other.file
+    }
+}
+
+class Piece extends Square {
+}
 
 export class QueenAttack {
     constructor(pieces= { white: [0, 3], black: [7, 3] }) {
+        // to make the tests happy
         this.white = pieces.white
         this.black = pieces.black
 
-        if (sameSpace(this.white, this.black)) {
+        if (this.whiteQueen.sameSquare(this.blackQueen)) {
             throw('Queens cannot share the same space')
         }
     }
+
+    get whiteQueen() {
+        return new Piece(this.white)
+    }
+
+    get blackQueen() {
+        return new Piece(this.black)
+    }
+
     toString() {
         var out = ""
-        for (let y=0; y < BOARD_EDGE; y++) {
-            for (let x=0; x < BOARD_EDGE; x++) {
-                out += this.squareAt(x,y) + " "
+        for (let rank=0; rank < BOARD_EDGE; rank++) {
+            for (let file=0; file < BOARD_EDGE; file++) {
+                out += this.pieceAt(new Square([rank,file])) + " "
             }
         out = out.trim() + "\n"
         }
         return out
     }
 
-    pieceAt(x,y, piece) {
-        return piece[0] == y && piece[1] == x
-    }
 
-    squareAt(x,y) {
-        if (this.pieceAt(x,y, this.white)) {
+    pieceAt(square) {
+        if (this.whiteQueen.sameSquare(square)) {
             return "W"
-        } else if (this.pieceAt(x,y, this.black)) {
+        } else if (this.blackQueen.sameSquare(square)) {
             return "B"
         } else { return "_" }
     }
 
     canAttack() {
-        return this.sameRow() || this.sameColumn() || this.sameDiagonal()
+        return this.whiteQueen.sameFile(this.blackQueen)
+            || this.whiteQueen.sameRank(this.blackQueen)
+            || this.whiteQueen.sameDiagonal(this.blackQueen)
     }
 
-    sameDiagonal() {
-        return Math.abs(this.white[0]-this.black[0]) ==
-            Math.abs(this.white[1]-this.black[1])
-    }
-
-    sameColumn() {
-        return this.white[1] == this.black[1]
-    }
-
-    sameRow() {
-        return this.white[0] == this.black[0]
-    }
 }
