21905c6c4f5e4a6c4532aa8586ee01dfed5b1795
Josh Goebel <me@joshgoebel.com>
2019-08-28 17:07:15 -0400

    simplify

--- forth/forth.js
+++ forth/forth.js
@@ -1,10 +1,11 @@
 
-const DIGIT = /[-+]?\d+/
-const OPERATOR = /^[+-/*]$/
-const DEFINE_FUNC = ":"
+const DIGIT_REGEX = /[-+]?\d+/
+const OPERATOR_REGEX = /^[+-/*]$/
+const DEFINE_FUNCTION = ":"
 const FUNCTION_END = ";"
 
-const digit = (x) => x.match(DIGIT)
+const isDigit = (x) => x.match(DIGIT_REGEX)
+const isOperator = (x) => x.match(OPERATOR_REGEX)
 
 const operations = {
     "+": "add",
@@ -20,24 +21,14 @@ const instructions = [
     "over"
 ]
 
-const PROGRAM_END = null
+const validInstruction = (token) =>  instructions.includes(token)
 
 class Parser {
     constructor(program) {
-        this.program = program
         this.stream = program.split(" ")[Symbol.iterator]()
-        this.current = this.stream.next()
     }
     fetch() {
-        let result = this.current.value
-        if (this.done()) {
-            return PROGRAM_END
-        }
-        this.current = this.stream.next()
-        return result
-    }
-    done() {
-        return this.current.done
+        return this.stream.next().value
     }
 }
 
@@ -51,46 +42,47 @@ export class Forth {
         let token
         this.parser = new Parser(code)
         while (token = this.parser.fetch()) {
-            if (token==PROGRAM_END) { return }
-
             this._eval(token)
         }
 
     }
 
     _eval(token) {
-        let lowered = token.toLowerCase()
-        if (digit(token)) {
+        token = token.toLowerCase()
+        if (isDigit(token)) {
             this.push(Number(token))
-        } else if (this.userFunctions[lowered]) {
-            this.execUserFunc(lowered)
-        } else if (token.match(OPERATOR)) {
-            this[operations[token]]()
-        } else if (instructions.includes(lowered)) {
-            this[lowered]()
-        } else if (token == DEFINE_FUNC) {
+        } else if (this.userFunctions[token]) {
+            this.execUserFunc(token)
+        } else if (isOperator(token)) {
+            this.execOp(token)
+        } else if (validInstruction(token)) {
+            this.execInst(token)
+        } else if (token == DEFINE_FUNCTION) {
             this.defineFunction()
         } else {
             throw(`Unknown command`)
         }
     }
 
+    execInst(token) {
+        this[`op_${token}`]()
+    }
+    execOp(token) {
+        this[`op_${operations[token]}`]()
+    }
+
     execUserFunc(name) {
         let func = this.userFunctions[name]
-        for (let inst of func ) {
-            this._eval(inst)
-        }
+        func.forEach(inst => this._eval(inst))
     }
 
     defineFunction() {
-        let token
-        let ops = []
+        let token, ops = []
         let name = this.parser.fetch().toLowerCase()
-        if (digit(name)) {
+        if (isDigit(name)) {
             throw('Invalid definition')
         }
-        while(token = this.parser.fetch()) {
-            if (token==FUNCTION_END) break;
+        while((token = this.parser.fetch()) != FUNCTION_END) {
             ops.push(token)
         }
         this.userFunctions[name] = ops
@@ -107,48 +99,48 @@ export class Forth {
         return this.stack.pop()
     }
 
-    get stackEmpty() {``
+    get stackEmpty() {
         return this.stack.length == 0
     }
 
     /* operations */
 
-    add() {
+    op_add() {
         this.push(this.pop() + this.pop())
     }
 
-    subtract() {
+    op_subtract() {
         let [a,b ] = [this.pop(), this.pop()]
         this.push(b - a)
     }
 
-    multiply() {
+    op_multiply() {
         this.push(this.pop() * this.pop())
     }
 
-    divide() {
-        let [b,a] = [this.pop(), this.pop()]
-        if (b==0) {
+    op_divide() {
+        let [dividend,divisor] = [this.pop(), this.pop()]
+        if (dividend==0) {
             throw('Division by zero')
         }
-        this.push(Math.floor(a/ b))
+        this.push(Math.floor(divisor/ dividend))
     }
 
-    dup() {
+    op_dup() {
         let a = this.pop()
         this.push(a, a)
     }
 
-    drop() {
+    op_drop() {
         this.pop()
     }
 
-    swap() {
+    op_swap() {
         let [b,a] = [this.pop(), this.pop()]
         this.push(b,a)
     }
 
-    over() {
+    op_over() {
         let [last,secondToLast] = [this.pop(), this.pop()]
         this.push(secondToLast,last,secondToLast)
     }
