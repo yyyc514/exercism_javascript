06d7ac6ceef33ccd19eaa488dae18dbc73e6abdb
Josh Goebel <me@joshgoebel.com>
2020-03-05 00:58:21 -0500

    simplify

--- word-search/word-search.js
+++ word-search/word-search.js
@@ -1,20 +1,3 @@
-//
-// This is only a SKELETON file for the 'Word Search' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// function reverseString(str) {
-//   return str.split("").reverse().join("");
-// }
-
-// function flip(result) {
-//   return {start: result.end, end: result.start }
-// }
-
-// function transpose(result) {
-//   return {start: result.start.reverse(), end: result.end.reverse() }
-// }
-
 const VECTORS = [
   [1,0], // left-to-right
   [-1,0], // right-to-left
@@ -33,8 +16,6 @@ function addVector(pos, vec) {
 class WordSearch {
   constructor(grid) {
     this.grid = grid
-    // transposed grid is used for top-bottom searches
-    // this.transposed = this.grid.map((_, i) => this.grid.map(el => el[i]).join(""))
   }
 
   find(words) {
@@ -44,22 +25,22 @@ class WordSearch {
     }
     return positions
   }
-  findWord(word) {
-    return this.findEvery(word)
-  }
-  charAt(x, y) {
+
+  letterAt(x, y) {
     if (!this.grid[y]) return undefined
     return this.grid[y][x]
   }
+
   wordAt(word, start, vector) {
     let pos = start
-    let [vecX,vecY] = vector
     for (let letter of word) {
-      if (letter !== this.charAt(...pos))
-        return false;
+      if (letter !== this.letterAt(...pos))
+        return undefined;
       pos = addVector(pos, vector)
     }
-    let len = (word.length-1)
+
+    let len = word.length - 1
+    let [vecX,vecY] = vector
     return {
       // reverse because tests want [y,x] coords
       // +[1,1] because tests top left is [1,1] not [0,0]
@@ -67,55 +48,33 @@ class WordSearch {
       end: addVector(start, [1 + vecX*len, 1 + vecY*len]).reverse()
     }
   }
-  findEvery(word) {
-    let result
+
+  // walks the grid but stops when `fn` returns true
+  walkGridFind(fn) {
     this.grid.find((_,y) => {
       return [...this.grid[0]].find((_, x) => {
-        // first make sure we match the first character
-        // to avoid a funch of needless searching
-        if (word[0] === this.charAt(x,y)) {
-          for (let vec of VECTORS) {
-            if (result = this.wordAt(word, [x,y], vec)) {
-              return result;
-            }
-          }
-        }
+        return fn(x,y)
       })
     })
-    return result === false ? undefined : result
   }
-  findBottomToTop(word) {
-    word = reverseString(word)
-    let result = this.findLeftToRight(word, this.transposed)
-    if (result) {
-      return transpose(flip(result))
-    }
-  }
-  findTopToBottom(word) {
-    let result = this.findLeftToRight(word, this.transposed)
-    if (result)
-      return transpose(result)
-  }
-  findRightToLeft(word) {
-    word = reverseString(word)
-    let result = this.findLeftToRight(word)
-    if (result) {
-      return flip(result)
-    }
-  }
-  findLeftToRight(word, grid = this.grid) {
+
+  findWord(word) {
     let result
-    grid.find((row, rowIndex) => {
-      let index = row.indexOf(word)
-      if (index!=-1) {
-        result = {
-          start: [rowIndex+1, index+1],
-          end: [rowIndex+1, index+word.length]
+    let firstLetter = word[0]
+    this.walkGridFind((x,y) => {
+      // first make sure we match the first character
+      // to avoid a bunch of needless vector searching
+      if (firstLetter !== this.letterAt(x,y)) return;
+
+      for (let vec of VECTORS) {
+        if ((result = this.wordAt(word, [x,y], vec))) {
+          return true;
         }
       }
     })
     return result
   }
+
 }
 
 export default WordSearch;
