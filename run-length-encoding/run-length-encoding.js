d43566d8740160f555b4abd392c5768d3b5e7e54
Josh Goebel <me@joshgoebel.com>
2020-03-06 03:09:37 -0500

    even better

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,22 +1,20 @@
-const encodeSequence = (sequence) => {
+const encodeSequence = (sequence, char) => {
   const repeat = sequence.length == 1 ? "" : sequence.length
-  return `${repeat}${sequence[0]}`
+  return `${repeat}${char}`
 }
 
-const decodeSequence = (sequence) => {
-  let length = parseInt(sequence)
-  let char = sequence.substr(-1)
+const decodeSequence = (_, length,char) => {
   return char.repeat(length)
 }
 
 const REPEATED_GROUPS_REGEX = /(.)\1*/g
-const ENCODED_REGEX = /\d+./g
+const ENCODED_REGEX = /(\d+)(.)/g
 
 export const encode = (phrase) => {
-  return phrase.replace(REPEATED_GROUPS_REGEX, (match) => encodeSequence(match)
+  return phrase.replace(REPEATED_GROUPS_REGEX, (...args) => encodeSequence(...args)
   )
 };
 
 export const decode = (phrase) => {
-  return phrase.replace(ENCODED_REGEX, (match) => decodeSequence(match))
+  return phrase.replace(ENCODED_REGEX, (...args) => decodeSequence(...args))
 };
