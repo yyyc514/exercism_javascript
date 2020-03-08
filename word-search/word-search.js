9dff1243174b22171bfc28609307c8c08c953f1c
Josh Goebel <me@joshgoebel.com>
2020-03-08 16:02:43 -0400

    clean it up

--- word-search/word-search.js
+++ word-search/word-search.js
@@ -1,36 +1,53 @@
 const VECTORS = [
-  [1,0], // left-to-right
-  [-1,0], // right-to-left
-  [0,1], // top-to-bottom
-  [0,-1], // bottom to top
-  [1,1], // top left to bottom right
-  [-1,-1], // bottom right to top left
-  [1,-1], // bottom left to top right
-  [-1,1] // top right to bottom left
+  {x:1 ,y:0},   // left-to-right
+  {x:-1 ,y:0},  // right-to-left
+  {x:0 ,y:1},   // top-to-bottom
+  {x:0 ,y:-1},  // bottom to top
+  {x:1 ,y:1},   // top left to bottom right
+  {x:-1 ,y:-1}, // bottom right to top left
+  {x:1 ,y:-1},  // bottom left to top right
+  {x:-1 ,y:1}   // top right to bottom left
 ]
-
-function addVector(pos, vec) {
-  return [pos[0] + vec[0], pos[1] + vec[1]]
-}
 
 class Grid {
   constructor(textGrid) {
     this.grid = textGrid
   }
 
-  letterAt(x, y) {
+  letterAt({x, y}) {
     if (!this.grid[y]) return undefined
     return this.grid[y][x]
   }
 
   // walks the grid but stops when `fn` returns true
-  traverseFind(fn) {
+  traverseUntilFind(fn) {
     this.grid.find((_,y) => {
       return [...this.grid[0]].find((_, x) => {
         return fn(x,y)
       })
     })
   }
+}
+
+function toOneBasedYXCoordSystem(cursor) {
+  let {x,y} = cursor
+  return [y+1,x+1]
+}
+
+const FORWARD = 1
+const BACKWARD = -1
+
+class Cursor {
+  constructor({x,y}, {direction} = {}) {
+    this.x = x
+    this.y = y
+    this.direction = direction
+  }
+  advance(flip = FORWARD) {
+    this.x += this.direction.x * flip
+    this.y += this.direction.y * flip
+  }
+  rewind() { this.advance(BACKWARD) }
 }
 
 class WordSearch {
@@ -46,34 +63,32 @@ class WordSearch {
     return positions
   }
 
-  wordAt(word, start, vector) {
-    let pos = start
+  wordAt(word, {x,y}, vector) {
+    let cursor = new Cursor({x,y}, { direction: vector })
+    cursor.rewind(); // get ready
     for (let letter of word) {
-      if (letter !== this.grid.letterAt(...pos))
-        return undefined;
-      pos = addVector(pos, vector)
+      cursor.advance();
+      if (letter !== this.grid.letterAt(cursor))
+        return;
     }
 
-    let len = word.length - 1
-    let [vecX,vecY] = vector
     return {
-      // reverse because tests want [y,x] coords
-      // +[1,1] because tests top left is [1,1] not [0,0]
-      start: addVector(start, [1,1]).reverse(),
-      end: addVector(start, [1 + vecX*len, 1 + vecY*len]).reverse()
+      // test want the coordinate system a very specific way, so convert it
+      start: [y+1, x+1],
+      end: toOneBasedYXCoordSystem(cursor),
     }
   }
 
   findWord(word) {
     let result
     let firstLetter = word[0]
-    this.grid.traverseFind((x,y) => {
+    this.grid.traverseUntilFind((x,y) => {
       // first make sure we match the first character
       // to avoid a bunch of needless vector searching
-      if (firstLetter !== this.grid.letterAt(x,y)) return;
+      if (firstLetter !== this.grid.letterAt({x,y})) return;
 
       for (let vec of VECTORS) {
-        if ((result = this.wordAt(word, [x,y], vec))) {
+        if (result = this.wordAt(word, {x,y}, vec)) {
           return true;
         }
       }
