e35d1eb653ae7b46d09c94036f16eecaa2ecf2c2
Josh Goebel <me@joshgoebel.com>
2020-03-12 01:32:53 -0400

    update

--- matching-brackets/matching-brackets.js
+++ matching-brackets/matching-brackets.js
@@ -1,34 +1,35 @@
-const openAndClosingBrackets = {
+const pairedBrackets = {
   "{": "}",
   "[": "]",
   "(": ")"
 }
 
-const opening = Object.keys(openAndClosingBrackets)
-const closing = Object.values(openAndClosingBrackets)
+const openingBrackets = Object.keys(pairedBrackets)
+const closingBrackets = Object.values(pairedBrackets)
+const isOpeningBracket = (c) => openingBrackets.includes(c)
+const isClosingBracket = (c) => closingBrackets.includes(c)
 
-const isOpeningBracket = (c) => opening.includes(c)
-const isClosingBracket = (c) => closing.includes(c)
+const bracketPair = (lastOpen, potentialClose) => {
+  let expectedClose = pairedBrackets[lastOpen]
+  return potentialClose === expectedClose
+}
 
 export const isPaired = (data) => {
-  let stack = []
-  for (let char of data) {
-    if (isOpeningBracket(char)) {
-      // console.log("open", char)
-      stack.push(char)
-    } else if (isClosingBracket(char)) {
-      let lastOpen = stack.pop()
-      let expectedClose = openAndClosingBrackets[lastOpen]
-      if (char != expectedClose) {
-        // console.log(`found ${char}, want ${expectedClose}`)
-        return false
-      }
+  const allBracketsAreClosed = () => openBrackets.length === 0
+  let openBrackets = []
+
+  for (let c of data) {
+    if (isOpeningBracket(c)) {
+      openBrackets.push(c)
+    } else if (isClosingBracket(c)) {
+      let lastOpen = openBrackets.pop()
+      if (!bracketPair(lastOpen, c))
+        // a single failure to match up means the whole string
+        // fails the pairing test
+        return false;
+    } else {
+      // if not a bracket, ignore character and proceed to next
     }
   }
-  if (stack.length > 0 )
-    return false
-
-  return true;
-
-  // throw new Error("Remove this statement and implement this function");
+  return allBracketsAreClosed()
 };
