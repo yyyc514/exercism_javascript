3f4e85c74c0463702d2115737976c4383e2a6db9
Josh Goebel <me@joshgoebel.com>
2020-03-16 22:21:06 -0400

    upgrade triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,56 +1,43 @@
-// experiment
-function unless(cond, func) {
-  if (!cond) { func() }
-}
-
-const not = (x) => !x
-const sortNumerics = (list) => list.sort( (a,b) => a-b )
-
 export class Triangle {
   constructor(a, b, c) {
-    self.sides = [a,b,c]
+    this.sides = [a,b,c]
   }
 
-  get matching_sides() {
-    let [a,b,c] = self.sides;
+  get matchingSides() {
+    let [a,b,c] = this.sides;
 
     if (a==b && b==c) {
       return 3
-    } else if (a==b || b==c) {
+    } else if (a==b || b==c || a==c) {
       return 2
     } else {
       return 0 // no matches
     }
   }
 
-  assertTriangleInequality() {
-    let [a,b,c] = sortNumerics(sides)
-    // unless(a+b >=c, () => throw("violates triangle inequality") )
+  passesTriangleInequality() {
+    let [a,b,c] = this.sides.sort()
 
-    if (not(a + b >= c)) {
-      throw "violates triangle inequality rules"
-    }
+    return a + b >= c
   }
 
-  assertValidSides() {
-    if (sides.some(side => side <= 0 )) {
-      throw "sides must have a length > 0"
-    }
+  hasValidSides() {
+    return this.sides.every(length => length > 0 );
   }
 
-  kind() {
-    this.assertValidSides();
-    this.assertTriangleInequality();
-
-    switch(this.matching_sides) {
-      case 3:
-        return "equilateral"
-      case 2:
-        return "isosceles"
-      case 0:
-        return "scalene"
-      default:
-        throw("logic error")
-    }
+  valid() {
+    return this.hasValidSides() &&
+      this.passesTriangleInequality()
   }
+
+  isEquilateral(){
+    return this.valid() && this.matchingSides === 3;
+  }
+  isIsosceles(){
+    return this.valid() && this.matchingSides >= 2;
+  }
+  isScalene(){
+    return this.valid() && this.matchingSides === 0;
+  }
+
 }
