f05df7a0808b68790efe860be0870d015fea8fbb
Josh Goebel <me@joshgoebel.com>
2020-04-04 18:36:33 -0400

    new solution

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -1,40 +1,81 @@
-const NAMED_OPERATIONS = {
+const throws = (err) => { throw err }
+const OPERATIONS = {
   "plus": (acc, n) => acc + n,
   "multiplied by": (acc, n) => acc * n,
   "minus": (acc, n) => acc - n,
   "divided by": (acc, n) => acc / n
 }
+const VALID_OPS = Object.keys(OPERATIONS)
 
-export class ArgumentError {
-}
-
-// A term is either "[word]* [number]" or simply "[word]"
-// the latter matches trailing words that usually indicate an error
-// for example: "What is 32" or "divided by -29".
-const TERM = /((?:[a-z]+\s?)+)\s([^ ?]+)|\w+/gi
-
-class Op {
-  constructor({op, value}) {
-    this.op = op;
-    this.value = Number(value);
+class TermStream {
+  constructor(stream) {
+    this.terms = stream
   }
-
-  calculate(acc) {
-    if (!NAMED_OPERATIONS[this.op]) throw new ArgumentError();
-    return NAMED_OPERATIONS[this.op](acc, this.value);
+  peek() {
+    return this.terms[0]
+  }
+  fetchNext() {
+    return this.terms.shift()
+  }
+  consumeNumber() {
+    return Number(this.fetchNext()) || null
+  }
+  consume(term) {
+    let current = this.fetchNext()
+    return current === term ? term : null
   }
 }
 
 export class WordProblem {
   constructor(question) {
-    // we always add the first value we find after "What is" so just hack the
-    // string vs hard code this in Op as an edge case
-    this.question = question.replace(/^What is/,"plus")
+    if (question.endsWith("?"))
+      question = question.replace("?","")
+
+    this.stream = new TermStream(question.split(" "))
   }
   answer() {
-    return [...this.question.matchAll(TERM)]
-      .map(x => new Op({ op: x[1], value: x[2]}))
-      .reduce((acc,op) => op.calculate(acc),0)
+    this.expect("What")
+    this.expect("is")
+
+    let acc = this.expectNumber();
+    while(this.more()) {
+      let [op, operand] = this.expectOperation();
+      acc = OPERATIONS[op](acc, operand)
+    }
+
+    return acc
+  }
+  expectOperation() {
+    return [this.expectOp(), this.expectNumber()]
+  }
+  expect(term) {
+    this.stream.consume(term) || throws(new Error("Unknown operation"))
+  }
+  expectNumber() {
+    return this.stream.consumeNumber() || throws(new Error("Syntax error"))
+  }
+  expandMultiWordOp(op) {
+    if (this.stream.peek()==="by") {
+      this.stream.fetchNext()
+      op = `${op} by`
+    }
+    return op
+  }
+  expectOp() {
+    let op = this.stream.fetchNext()
+    // for some reason we want a numeric error if the op is a number
+    // vs an invalid opcode error - blame it on the specs
+    if (Number(op)) throw new Error("Syntax error")
+
+    op = this.expandMultiWordOp(op)
+    if (!VALID_OPS.includes(op)) throw new Error("Unknown operation")
+    return op
+  }
+  more() {
+    return this.stream.peek()
   }
 }
 
+export const answer = (question) => {
+  return new WordProblem(question).answer()
+}
